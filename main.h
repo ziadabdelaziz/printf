a037b156c9c35d2fc841bdc6060a3c49743099e7
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-28 08:03:42 -0700

    init

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void _putchar(int c);
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+int _printf(const char *format, ...)
+{
+
+
+}
