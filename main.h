1f0213e889b498f101a05cc02d27789d6654a2d2
ziadabdelaziz <ziadabdelaziz1995@gmail.com>
2023-03-28 08:50:23 -0700

    handle %, c, s

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 void _putchar(int c);
 int _printf(const char *format, ...);
--- printf.c
+++ printf.c
@@ -1,9 +1,46 @@
 #include "main.h"
-#include <stdlib.h>
 
-
+/**
+* _printf - printf function
+* @format: formated string
+* Return: number of printed characters
+*/
 int _printf(const char *format, ...)
 {
+	int cnt, i, j;
+	char *str;
+	va_list types;
 
+	va_start(types, format);
+	cnt = 0;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch(format[i + 1])
+			{
+				case 'c':
+					_putchar(va_arg(types, int));
+					break;
 
+				case 's':
+					str = va_arg(types, char *);
+					for (j = 0; str[j] != '\0'; j++)
+					{
+						_putchar(str[j]);
+						cnt++;
+					}
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+		cnt++;
+	}
+
+	va_end(types);
+
+	return (cnt);
 }
